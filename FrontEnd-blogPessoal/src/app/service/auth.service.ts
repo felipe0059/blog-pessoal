f14913016be0c39a85bcd07b81a0dc20a783600e
felipe0059 <felipe.nx4@gmail.com>
2021-09-06 19:15:18 -0300

    Front Funcional

---
+++ FrontEnd-blogPessoal/src/app/service/auth.service.ts
@@ -0,0 +1,38 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { Usuario } from '../model/Usuario';
+import { UsuarioLogin } from '../model/UsuarioLogin';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  entrar(usuarioLogin: UsuarioLogin): Observable<UsuarioLogin>{
+  return this.http.post<UsuarioLogin>('https://blogfelipegustavo.herokuapp.com/usuarios/login', usuarioLogin)
+  }
+
+  cadastrar (usuario: Usuario): Observable<Usuario>{
+    return this.http.post<Usuario>('https://blogfelipegustavo.herokuapp.com/usuarios/register', usuario)
+
+  }
+
+  getByIdUsuario(id: number): Observable<Usuario>{
+    return this.http.get<Usuario>(`https://blogfelipegustavo.herokuapp.com/usuarios/${id}`)
+  }
+
+  logado(){
+    let ok: boolean = false
+
+    if(environment.token != ''){
+      ok = true
+    }
+    return ok
+  }
+}
